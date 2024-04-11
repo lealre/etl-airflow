bc43728fe0ef6a59d405d222d091c29ce6b64ac9
lealre <renaanvp@gmail.com>
2024-04-11 21:44:11 +0100

    feat: All etl steps running in Airflow

--- dags/dag_etl.py
+++ dags/dag_etl.py
@@ -1,11 +1,11 @@
-from src.etl import extract_files, load_files
+from src.etl import connect_drive_and_extract_files, extract_data_from_files, transform_data, load_files
 from airflow.decorators import dag, task
 from datetime import datetime
 
 @dag(
-        dag_id = 'example_dag',
-        description='Dag with 3 simple steps',
-        schedule= '* * * * *',
+        dag_id = 'ETL-GDrive-to-PostgreSQL',
+        description='Dag to extract data from Google Drive, transform and store in a POstgreSQL',
+        schedule= '*/2 * * * *',
         start_date= datetime(2024,4,7),
         catchup= False,
         params= {
@@ -17,18 +17,28 @@ from datetime import datetime
 )
 def pipeline():
 
-    @task(task_id = 'extract-data')
-    def task_extract_files(**context):
-        return extract_files(context['params']['service_account_path'], context['params']['parent_folder_name'], context['params']['folder_to_extract'])
+    @task(task_id = 'connect-with-drive-and-read-files-info')
+    def task_connect_drive_and_extract_files(**context):
+        return connect_drive_and_extract_files(context['params']['service_account_path'], context['params']['parent_folder_name'], context['params']['folder_to_extract'])
     
+    @task(task_id = 'extract-data')
+    def task_extract_data_from_files(list_df):
+        return extract_data_from_files(list_df)
+    
+    @task(task_id = 'transform-data')
+    def task_transform_data(list_df):
+        return transform_data(list_df)
+
     @task(task_id = 'load-data')
     def task_load_files(list_df):
         return load_files(list_df)
     
-    task_extract = task_extract_files()
-    task_load = task_load_files(task_extract) 
+    task_connect_and_extract = task_connect_drive_and_extract_files()
+    task_extract_data = task_extract_data_from_files(task_connect_and_extract)
+    task_transform = task_transform_data(task_extract_data)
+    task_load = task_load_files(task_transform) 
 
-    task_extract >> task_load
+    task_connect_and_extract >> task_extract_data >> task_transform >> task_load
 
 
 pipeline()
