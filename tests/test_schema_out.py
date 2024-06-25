9a21a8163ab3b23d072ccecaa8a93e4d74969748
lealre <renaanvp@gmail.com>
2024-06-25 21:17:09 +0100

    test: set pytest path in pyproject.toml

--- tests/test_schema_out.py
+++ tests/test_schema_out.py
@@ -1,7 +1,3 @@
-import sys
-import os
-# Add the root directory of your project to sys.path
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import pytest
 import pandera as pa
 import pandas as pd
