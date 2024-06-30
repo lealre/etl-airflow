5e08064a70e738e732420db9daab0ce5db6a41a9
lealre <renaanvp@gmail.com>
2024-06-30 14:21:43 +0100

    feat: set default PostgreSQL connection to Docker localhost. Render is still an option by passing the variables through .env.

--- src/database.py
+++ src/database.py
@@ -7,11 +7,11 @@ from typing import Union
 
 load_dotenv(".env")
 
-POSTGRES_USER = os.getenv('POSTGRES_USER')
-POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD')
-POSTGRES_HOST = os.getenv('POSTGRES_HOST')
-POSTGRES_PORT = os.getenv('POSTGRES_PORT')
-POSTGRES_DB = os.getenv('POSTGRES_DB')
+POSTGRES_USER = os.getenv('POSTGRES_USER', 'user-name')
+POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD', 'postgres')
+POSTGRES_HOST = os.getenv('POSTGRES_HOST', 'localhost')
+POSTGRES_PORT = os.getenv('POSTGRES_PORT', 5432)
+POSTGRES_DB = os.getenv('POSTGRES_DB', 'mydb')
 
 POSTGRES_DATABASE_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
 
@@ -19,14 +19,14 @@ engine = create_engine(POSTGRES_DATABASE_URL)
 
 
 def get_files_ids_from_db() -> Union[list[str], list[None]]:
+	
+	table_name = 'revenues'    
+	metadata = MetaData(bind=engine)
+	metadata.reflect()
 
-    table_name = 'revenues'    
-    metadata = MetaData(bind=engine)
-    metadata.reflect()
-
-    if table_name in metadata.tables: 
-            query = f"SELECT DISTINCT file_id FROM {table_name}"
-            database_gd_ids = pd.read_sql_query(query, engine)
-            return list(database_gd_ids['file_id'])
-
-    return []
+	if table_name in metadata.tables: 
+		query = f"SELECT DISTINCT file_id FROM {table_name}"
+		database_gd_ids = pd.read_sql_query(query, engine)
+		return list(database_gd_ids['file_id'])
+	
+	return []	
